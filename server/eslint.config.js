fa2ebdc49d024675beb446c9f908b432bb541ccb
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-12-28 22:43:47 +0700

    Disabling linting during production builds

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,8 +1,33 @@
+// import globals from 'globals'
+// import pluginJs from '@eslint/js'
+
+// /** @type {import('eslint').Linter.Config[]} */
+// export default [
+// 	{ languageOptions: { globals: globals.browser } },
+// 	pluginJs.configs.recommended,
+// ]
+
+//////////////////////////////////////////////////////
 import globals from 'globals'
 import pluginJs from '@eslint/js'
+import pluginReact from 'eslint-plugin-react'
+import pluginImport from 'eslint-plugin-import'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	{ languageOptions: { globals: globals.browser } },
+	{
+		languageOptions: {
+			globals: globals.browser,
+		},
+		plugins: {
+			react: pluginReact,
+			import: pluginImport,
+		},
+		rules: {
+			'react/react-in-jsx-scope': 'off',
+			'import/no-unresolved': 'error',
+			ignoreDuringBuilds: 'true',
+		},
+	},
 	pluginJs.configs.recommended,
 ]
